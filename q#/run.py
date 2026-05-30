2b1a08e2fcb28354e4ff93d8d88d1545ab56b59d
Yotta11 <nguiyoumerveille@gmail.com>
2026-05-30 17:43:06 +0100

    feat:commentaires

--- q#/run.py
+++ q#/run.py
@@ -1,10 +1,11 @@
+#projet d'implementation du tri bitnique et du minage de la blockchain'
+
 import qsharp
 
 # Initialise le projet
 qsharp.init(project_root = ".")
 
 print("--- Lancement du Devoir Quantique ---")
-
 # qsharp.eval() renvoie les sorties Message() directement vers la console
 # dans la plupart des environnements. 
 # Si vous ne voyez rien, c'est peut-être que l'opération ne retourne rien.
@@ -13,5 +14,4 @@ try:
     qsharp.eval("QuantumComputing.ExecuteQuantumTasks()")
 except Exception as e:
     print(f"Erreur d'exécution : {e}")
-
 print("--- Simulation terminée ---")
